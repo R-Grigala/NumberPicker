69905d15b19ff04bec0825a781228907f4a5547d
R-Grigala <r.grigalashvili777@gmail.com>
2023-10-27 00:07:38 +0400

    Coding Grigala:// Olaaaa

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const items = [{
-      0:"0️⃣",
-      1:"1️⃣",
-      2:"2️⃣",
-      3:"3️⃣",
-      4:"4️⃣",
-      5:"5️⃣",
-      6:"6️⃣",
-      7:"7️⃣",
-      8:"8️⃣",
-      9:"9️⃣"
+      0:"./assets/images/emoji_0.png",
+      1:"./assets/images/emoji_1.png",
+      2:"./assets/images/emoji_2.png",
+      3:"./assets/images/emoji_3.png",
+      4:"./assets/images/emoji_4.png",
+      5:"./assets/images/emoji_5.png",
+      6:"./assets/images/emoji_6.png",
+      7:"./assets/images/emoji_7.png",
+      8:"./assets/images/emoji_8.png",
+      9:"./assets/images/emoji_9.png"
 }];
 
-    const joinedText = Object.values(items[0]).join(" ");
-    document.querySelector(".info").textContent = joinedText;
+    const joinedImages = Object.values(items[0]).map(url => `<img src="${url}" />`).join(" ");
+    document.querySelector(".info").innerHTML = joinedImages;
   
     const doors = document.querySelectorAll(".door");
     document.querySelector("#spinner").addEventListener("click", spin);
@@ -63,70 +63,70 @@ const items = [{
     function init(firstInit = true, groups = 1, duration = 1) {
       const randomValue = anotherFunction();
       let numArray = randomValue.toString().split('').map(Number);
+    
       for (let j = 0; j < doors.length; j++) {
         const door = doors[j];
-        // console.log(door)
         if (firstInit) {
           door.dataset.spinned = "0";
         } else if (door.dataset.spinned === "1") {
           return;
         }
-  
+    
         const boxes = door.querySelector(".boxes");
         const boxesClone = boxes.cloneNode(false);
-  
-        const pool = ["0️⃣"];
+    
+        const pool = ["./assets/images/emoji_0.png"];
         if (!firstInit) {
           const arr = [];
           for (let n = 0; n < (groups > 0 ? groups : 1); n++) {
             arr.push(...Object.values(items[0]));
           }
-
+    
           pool.push(...shuffle(arr, numArray[j])); // Call the shuffle function with the current digit
-          
+    
           boxesClone.addEventListener(
             "transitionstart",
             function () {
               door.dataset.spinned = "1";
-              this.querySelectorAll(".box").forEach((box) => {
-                box.style.filter = "blur(1px)";
+              this.querySelectorAll(".box").forEach((img) => {
+                img.style.filter = "blur(1px)";
               });
             },
             { once: true }
           );
-  
+    
           boxesClone.addEventListener(
             "transitionend",
             function () {
-              this.querySelectorAll(".box").forEach((box, index) => {
-                box.style.filter = "blur(0)";
-                if (index > 0) this.removeChild(box);
+              this.querySelectorAll(".box").forEach((img, index) => {
+                img.style.filter = "blur(0)";
+                if (index > 0) this.removeChild(this.children[0]);
               });
             },
             { once: true }
           );
         }
-  
+    
         for (let i = pool.length - 1; i >= 0; i--) {
-          const box = document.createElement("div");
-          box.classList.add("box");
-          box.style.width = door.clientWidth + "px";
-          box.style.height = door.clientHeight + "px";
-          box.textContent = pool[i];
+          const box = document.createElement("img");
+          box.src = pool[i];
+          box.style.width = "100%";
+          box.style.height = "100%";
+    
           boxesClone.appendChild(box);
         }
+    
         boxesClone.style.transitionDuration = `${duration > 0 ? duration : 1}s`;
-        boxesClone.style.transform = `translateY(-${
-          door.clientHeight * (pool.length - 1)
-        }px)`;
+        boxesClone.style.transform = `translateY(-${door.clientHeight * (pool.length - 1)}px)`;
         door.replaceChild(boxesClone, boxes);
       }
     }
+    
   
     function shuffle([...arr], numArray) {
 
       arr[arr.length - 1]= arr[numArray]
-      
+      console.log(arr)
       return arr;
     }
   
